4aa8a582f1176d5373141de317afeeb8a8409b3d
Demidog03 <olzhasktl362@gmail.com>
2024-01-22 23:48:19 +0600

    feat: JjxWNLAB, adding navbar component

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,15 @@
+import {FC} from 'react'
+
+interface MainPageProps {
+
+}
+
+const MainPage: FC<MainPageProps> = () => {
+  return (
+      <div className="bg-green-300 text-[24px]">
+
+      </div>
+  )
+}
+
+export default MainPage
