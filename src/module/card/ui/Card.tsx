266fedabca8a0a5b22f616d71ce7fa8a83785788
Ayan.Abzhakov <Ayan.Abzhakov@isate.kz>
2024-02-01 01:40:17 +0600

    feat: 5sMngZLr v3

--- src/module/card/ui/Card.tsx
+++ src/module/card/ui/Card.tsx
@@ -1,44 +1,36 @@
 import React from 'react';
 
 interface ConfigurableCardProps {
-    cardColor?: 'f9ecf9' | 'e6f4ff';
-    textBoxStyles?: string;
-    imageBoxStyles?: string;
-    title?: string;
-    text?: string;
-    textStyles?: string;
-    imagePosition?: 'left' | 'right';
-    imageSrc?: string;
-    imageStyles?: string;
+    bgColor: 'pink' | 'blue';
+    title: string;
+    text: string;
+    imagePosition: 'left' | 'right';
+    imageSrc: string;
 }
 
-const ConfigurableCard: React.FC<ConfigurableCardProps> = ({   cardColor,
-                                                               textBoxStyles,
-                                                               imageBoxStyles,
+const ConfigurableCard: React.FC<ConfigurableCardProps> = ({   bgColor,
                                                                title,
                                                                text,
-                                                               textStyles,
                                                                imagePosition,
                                                                imageSrc,
-                                                               imageStyles
 }) => {
     const isImageOnLeft = imagePosition === 'left';
-    const isPinkCard = cardColor === 'f9ecf9';
+    const isPinkCard = bgColor === 'pink';
 
     return (
         <div
             className={`h-4 w-8 p-2 flex items-center justify-center bg-gradient-to-b from-${isPinkCard? 'f9ecf9' : 'e6f4ff'} to-ffffff`}>
-            <div className={`${textBoxStyles} ${isImageOnLeft ? 'order-2 pl-6' : 'pr-6'}`}>
+            <div className={`${isImageOnLeft ? 'order-2 pl-6' : 'pr-6'}`}>
                 <h5 className="text-black text-22">
                     {title}
                 </h5>
-                <p className={`${textStyles} text-10`}>
+                <p className="text-10">
                     {text}
                 </p>
             </div>
-            <div className={`${imageBoxStyles} ${isImageOnLeft ? 'order-1' : 'order-3'} p-2`}>
+            <div className={`${isImageOnLeft ? 'order-1' : 'order-3'} p-2`}>
                 <img
-                    className={`${imageStyles} max-h-12 max-w-12`}
+                    className="max-h-12 max-w-12"
                     src={imageSrc}
                     alt={title}
                 />
