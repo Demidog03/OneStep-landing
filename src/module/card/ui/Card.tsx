687796d4d0a38e843b27d298b616c16950037fb3
Ayan.Abzhakov <Ayan.Abzhakov@isate.kz>
2024-02-01 01:32:35 +0600

    feat: 5sMngZLr

--- src/module/card/ui/Card.tsx
+++ src/module/card/ui/Card.tsx
@@ -1,44 +1,44 @@
 import React from 'react';
 
 interface ConfigurableCardProps {
-    cardStyles?: string;
+    cardColor?: 'f9ecf9' | 'e6f4ff';
     textBoxStyles?: string;
     imageBoxStyles?: string;
     title?: string;
     text?: string;
-    titleStyles?: string;
     textStyles?: string;
     imagePosition?: 'left' | 'right';
     imageSrc?: string;
     imageStyles?: string;
 }
 
-const ConfigurableCard: React.FC<ConfigurableCardProps> = ({   cardStyles,
+const ConfigurableCard: React.FC<ConfigurableCardProps> = ({   cardColor,
                                                                textBoxStyles,
                                                                imageBoxStyles,
                                                                title,
                                                                text,
-                                                               titleStyles,
-                                                               textStyles ,
+                                                               textStyles,
                                                                imagePosition,
                                                                imageSrc,
                                                                imageStyles
 }) => {
     const isImageOnLeft = imagePosition === 'left';
+    const isPinkCard = cardColor === 'f9ecf9';
 
     return (
-        <div className={cardStyles}>
+        <div
+            className={`h-4 w-8 p-2 flex items-center justify-center bg-gradient-to-b from-${isPinkCard? 'f9ecf9' : 'e6f4ff'} to-ffffff`}>
             <div className={`${textBoxStyles} ${isImageOnLeft ? 'order-2 pl-6' : 'pr-6'}`}>
-                <h5 className={titleStyles}>
+                <h5 className="text-black text-22">
                     {title}
                 </h5>
-                <p className={textStyles}>
+                <p className={`${textStyles} text-10`}>
                     {text}
                 </p>
             </div>
-            <div className={`${imageBoxStyles} ${isImageOnLeft ? 'order-1' : 'order-3'}`}>
+            <div className={`${imageBoxStyles} ${isImageOnLeft ? 'order-1' : 'order-3'} p-2`}>
                 <img
-                    className={imageStyles}
+                    className={`${imageStyles} max-h-12 max-w-12`}
                     src={imageSrc}
                     alt={title}
                 />
